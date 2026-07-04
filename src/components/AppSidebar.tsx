ba9c5e812e79c800a955bb2225d01479968b6909
yoyovirus <raulkhedekar@gmail.com>
2026-07-05 01:34:02 +0530

    Update AppSidebar.tsx

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -147,7 +147,6 @@ export function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
                     </div>
                 )}
             </SidebarContent>
-            <SidebarRail />
         </Sidebar>
     );
 }
